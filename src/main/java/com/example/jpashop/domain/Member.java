a429dad65ae03ef75473c4abd8f612bbfd2988ae
Lee-jeong-wook <b01024134020@gmail.com>
2024-04-09 18:26:56 +0900

    user add, edit, view api created

--- src/main/java/com/example/jpashop/domain/Member.java
+++ src/main/java/com/example/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package com.example.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +16,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded

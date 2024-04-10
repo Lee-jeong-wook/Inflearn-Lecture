d8f0890d4194ba9c64dda0c0cd24a16d3e2a4750
Lee-jeong-wook <b01024134020@gmail.com>
2024-04-10 22:13:34 +0900

    orders v2, v3 created

--- src/main/java/com/example/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/com/example/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,19 @@
 package com.example.jpashop.api;
 
+import com.example.jpashop.domain.Address;
 import com.example.jpashop.domain.Order;
+import com.example.jpashop.domain.OrderStatus;
 import com.example.jpashop.repository.OrderRepository;
 import com.example.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOner, OneToOne)
@@ -30,5 +36,35 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        List<SimpleOrderDto> result = orderRepository.findAllByCriteria((new OrderSearch())).stream().map(o -> new SimpleOrderDto(o)).collect((Collectors.toList()));
+        return result;
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3(){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream().map(o -> new SimpleOrderDto(o)).collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName(); //lazy 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //lazy 초기화
+        }
     }
 }
